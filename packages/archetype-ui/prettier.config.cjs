1942a861e01eb18e451bad2b0337d5d5eb5b4356
Dani <hi@daniguardio.la>
2023-08-19 07:27:08 +0200

    test

--- packages/archetype-ui/prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    require("prettier-plugin-tailwindcss"),
-    require("prettier-plugin-jsdoc"),
-  ],
-  jsdocDescriptionWithDot: true,
-  tsdoc: true,
-};
--- packages/archetype-ui/src/text-field/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./FileInput";
-export * from "./TextField";
